715f4612758ccf4ca4db7f102adcb1fd931b768b
andretassis <tassisandre@gmail.com>
2023-08-08 10:54:52 -0300

    editado css do componente de livro

--- src/app/componentes/estante/livro/livro.component.ts
+++ src/app/componentes/estante/livro/livro.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-livro',
   templateUrl: './livro.component.html',
   styleUrls: ['./livro.component.css']
 })
 export class LivroComponent implements OnInit {
+
 
   constructor() { }
 
